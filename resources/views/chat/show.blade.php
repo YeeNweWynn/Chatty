4c5c9f7d463bfa25c81f108009dfe4fd6c0d7a06
yeenwewynn <yeenwewynn@gmail.com>
2018-10-15 09:38:43 +0630

    show

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<meta name="friendId" content="{{$friend->id}}">
 <div class="container">
 	<div class="row justify-content-center">
 	    <div class="col-md-8">
